fc64ce60d61025f395567f789da90a611d78bc29
Christian Kokott <christian.kokott@online.de>
2013-04-23 23:17:19 +0200

    basic friction testing

--- YaSaog/YaSaog/Entities/Bubble.cs
+++ YaSaog/YaSaog/Entities/Bubble.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace YaSaog.Entities {
 
     public class Bubble : BaseEntity {
 
         private Vector2 velocity { get; set; }
+        private Vector2 friction { get; set; }
 
         public Bubble(int x, int y) : base() {           
             X = x;
@@ -17,7 +19,8 @@ namespace YaSaog.Entities {
 
             Size = new Vector2(75, 75);
 
-            velocity = new Vector2(0, 20f);
+            velocity = new Vector2(0f, 50f);
+            friction = new Vector2(0.2f, 0.2f);
         }
 
         public override void Init() {        
@@ -25,6 +28,16 @@ namespace YaSaog.Entities {
 
         public override void Update(GameTime gameTime) {
             base.Update(gameTime);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) velocity += new Vector2(2f, 0);
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) velocity += new Vector2(-2f, 0);
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) velocity += new Vector2(0, -2f);
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) velocity += new Vector2(0, 2f);
+
+            float _x = Math.Max(0, Math.Abs(velocity.X - friction.X));
+            float _y = Math.Max(0, Math.Abs(velocity.Y - friction.Y));            
+
+            velocity = new Vector2(_x * Math.Sign(velocity.X), _y * Math.Sign(velocity.Y));
 
             X += velocity.X * (float)gameTime.ElapsedGameTime.TotalSeconds;
             Y += velocity.Y * (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -34,8 +47,7 @@ namespace YaSaog.Entities {
             spriteBatch.Draw(Assets.BubbleBlue, BoundingBox, null, Color.White, 0, new Vector2(Assets.BubbleBlue.Width / 2, Assets.BubbleBlue.Height / 2), SpriteEffects.None, 0);
         }
 
-        public override void Delete() {
-            throw new NotImplementedException();
+        public override void Delete() {            
         }
     }
 }
