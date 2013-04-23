8ecef57d19f2f46356b3dc1609e0c092a6c5d4c4
Christian Kokott <christian.kokott@online.de>
2013-04-23 18:06:12 +0200

    xna init

---
+++ YaSaog/YaSaog/MainGame.cs
@@ -0,0 +1,43 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace YaSaog {
+
+    public class MainGame : Game {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public MainGame() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize() {
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            spriteBatch = new SpriteBatch(GraphicsDevice);            
+        }
+
+        protected override void UnloadContent() {
+
+        }
+
+        protected override void Update(GameTime gameTime) {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ YaSaog/YaSaog/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace YaSaog {
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (MainGame game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
